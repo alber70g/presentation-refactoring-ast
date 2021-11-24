814e4b64d0a4a8e7d396838b5b7e9e5ad9433198
Albert Groothedde <albertgroothedde@gmail.com>
2021-11-24 15:43:57 +0100

    minor changes

--- sample-project/src/helpers/helpers.ts
+++ sample-project/src/helpers/helpers.ts
@@ -1,17 +1,16 @@
 import * as _ from 'lodash';
-// import only what's used
-// import from 'lodash-es'
 
-// modify `function() {}` into `() => {}`
+// extract functions into their own files, and modify imports
+
 export function sanitizeNumbersArray(arr: (string | number)[]) {
   _.each(arr, function (value) {
+    // use parseFloat for parsing strings to float
     return +value;
   });
 }
 
-// use parseFloat for parsing strings to float
 export function parseStringToNumber(str: string) {
+  // use parseFloat for parsing strings to float
   return +str;
 }
 
-// extract functions into their own files, and modify imports
